3a239d1e5bca8d1c41156f802c8a7718dd293671
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-05-07 23:03:25 +0600

    Problem solved

---
+++ solutions.ts
@@ -0,0 +1,60 @@
+const filterEvenNumbers = (arr: number[]): number[] => {
+  return arr.filter((num) => num % 2 === 0);
+};
+
+const reverseString = (str: string): string => {
+  return str.split("").reduce((reversed, char) => char + reversed, "");
+};
+
+type stringOrNumber = string | number;
+const checkType = (value: stringOrNumber): string => {
+  if (typeof value === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+};
+
+const getProperty = <T, key extends keyof T>(obj: T, key: key) => {
+  return obj[key];
+};
+
+
+interface Book{
+    title:string;
+    author:string;
+    publishedYear:number;
+
+}
+
+const toggleReadStatus =(book:Book)=>{
+    return{
+        ...book,
+        isRead:true,
+    };
+};
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  const set1 = new Set(arr1);
+
+  return arr2.filter((item) => set1.has(item));
+}
+
